c020a650423fd95e3e71d8b0e5b252ec885d5ac1
Bibi210 <brahimadibassi@gmail.com>
2023-10-10 21:03:42 +0200

    Dead End

--- lib/typeur.ml
+++ lib/typeur.ml
@@ -54,3 +54,46 @@ let generateTypeEquations tree =
   let rtree = generateTypeEquations' t_var Env.empty tree in
   rtree
 ;;
+
+let rec occursCheck var t =
+  match t.tpre with
+  | TVar x when x = var -> true
+  | TLambda { varg; tbody } -> occursCheck var varg || occursCheck var tbody
+  | _ -> false
+;;
+
+let rec substitute typ tvar newtyp =
+  match typ.tpre with
+  | TVar x when x = tvar -> newtyp
+  | TLambda { varg; tbody } ->
+    let newvarg = substitute varg tvar newtyp in
+    let newtbody = substitute tbody tvar newtyp in
+    { typ with tpre = TLambda { varg = newvarg; tbody = newtbody } }
+  | TEquationEqual (t1, t2) ->
+    let newt1 = substitute t1 tvar newtyp in
+    let newt2 = substitute t2 tvar newtyp in
+    { typ with tpre = TEquationEqual (newt1, newt2) }
+  | _ -> typ
+;;
+
+let rec substituteAndWrite tree tvar newtyp =
+  let newType = substitute tree.etype tvar newtyp in
+  let newExpr =
+    match tree.epre with
+    | Var _ -> tree.epre
+    | Lambda lmb ->
+      let newbody = substituteAndWrite lmb.body tvar newtyp in
+      Lambda { lmb with body = newbody }
+    | App app ->
+      let newfunc = substituteAndWrite app.func tvar newtyp in
+      let newcarg = substituteAndWrite app.carg tvar newtyp in
+      App { func = newfunc; carg = newcarg }
+  in
+  { tree with etype = newType; epre = newExpr }
+;;
+
+
+(* TODO Passer a une liste d'équation type equation = ptype * ptype) list *)
+(* TODO Change l'ast pour stock des Options equation *)
+(* TODO Remove TEqEquation *)
+(* TODO Fix Using Teacher stuff *)
